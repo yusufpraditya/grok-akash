57848bf76362b0297fc205a818e95376d4024e04
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2024-03-23 13:48:02 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 huggingface-cli download hpcai-tech/grok-1 --repo-type model --local-dir hpcaitech/grok-1 --local-dir-use-symlinks False
+
 pip install accelerate
+
+cd /ColossalAI/examples/language/grok-1
 
 gotty -w "python entrypoint.py"
